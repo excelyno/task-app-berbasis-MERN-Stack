7b9a8fa7721245a222879cfbf7cb29424082fb52
Excelyno Magenta <excellentqweee@gmail.com>
2026-01-19 19:38:24 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,12 +2,10 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    // Pastikan dia baca process.env.MONGO_URI, BUKAN string hardcoded!
     const conn = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-    // 👇 TAMBAHKAN BARIS INI UNTUK MELIHAT NAMA DATABASE 👇
-    console.log(`📂 Nama Database Aktif: ${conn.connection.name}`); 
+    console.log(`Nama Database Aktif: ${conn.connection.name}`); 
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
